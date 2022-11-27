51d8e1aada4b63f78ee150a608e0066516baaa8a
Trexer1010 <102918578+Trexer1010@users.noreply.github.com>
2022-11-28 02:09:54 +0900

    Add files via upload

---
+++ list.js
@@ -0,0 +1,74 @@
+let allMusic = [
+    {
+        name : "너를 생각해",
+        artist : "주시크",
+        img : "img-1",
+        audio : "m-1"
+    },
+    {
+        name : "Constant Moderato (Blue Archive)",
+        artist : "Mitsukiyo",
+        img : "img-2",
+        audio : "m-2"
+    },
+    {
+        name : "사랑이란 멜로는 없어",
+        artist : "전상근",
+        img : "img-3",
+        audio : "m-3"
+    },
+    {
+        name : "서툰 이별을 하려해 (Feat.전상근)",
+        artist : "윤토벤",
+        img : "img-4",
+        audio : "m-4"
+    },
+    {
+        name: "금지된 사랑 (원곡 : 김경호)",
+        artist : "이승기",
+        img : "img-5",
+        audio : "m-5"
+    },
+    {
+        name: "안녕 우린 헤어져야만 해",
+        artist : "전상근, 신예영",
+        img : "img-6",
+        audio : "m-6"
+    },
+    {
+        name: "When I Get Old",
+        artist : "크리스토퍼, 청하",
+        img : "img-7",
+        audio : "m-7"
+    },
+    {
+        name: "After Rollin (After Like X Rollin)",
+        artist : "IVE, Brave Girls, SS2S LEE",
+        img : "img-8",
+        audio : "m-8"
+    },
+    {
+        name: "Loving You Girl (ft. Hkeem)",
+        artist : "Peder Elias",
+        img : "img-9",
+        audio : "m-9"
+    },
+    {
+        name: "언제나 사랑해 (원곡 : 제이세라)",
+        artist : "케이시",
+        img : "img-10",
+        audio : "m-10"
+    },
+    {
+        name: "내가 아니라도 (Live Ver.)",
+        artist : "주호",
+        img : "img-11",
+        audio : "m-11"
+    },
+    {
+        name: "사랑했었다",
+        artist : "이홍기, 유회승",
+        img : "img-12",
+        audio : "m-12"
+    }
+]
